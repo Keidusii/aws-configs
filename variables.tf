c9f889aa297fe3b25c723f7ea4f3e9325092cc15
Markeidus Hall II <mhallii@umich.edu>
2023-08-28 19:28:15 -0400

    aws config repo init

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "cluser_name" {
+  description = "Name for EKS cluster"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "RDS root user password"
+  type        = string
+  sensitive   = true
+}
